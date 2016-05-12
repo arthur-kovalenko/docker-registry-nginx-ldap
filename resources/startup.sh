e915ec54c98bd701e0e0b4a1cd7f550521418009
Kovalenko <arturs.kovalenko@accenture.com>
2016-05-12 09:24:12 +0300

    Added SERVER_IP parameter

--- resources/startup.sh
+++ resources/startup.sh
@@ -8,21 +8,24 @@ echo "Private IP address for nginx proxy container is $PRIVATE_IP"
 # Function to generate new self-signed certificates for proxy container
 function Generate_Certificates {
 	# Get the public IP address for the container, if none is specified
+	SERVER_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 	if [ -z $ROOT_COMMON_NAME ]; then
-		ROOT_COMMON_NAME=$(dig +short myip.opendns.com @resolver1.opendns.com)
+		ROOT_COMMON_NAME="registry.${SERVER_IP}.xip.io"
 	fi
-	sed -i "s/###IP###/$ROOT_COMMON_NAME/g" /etc/ssl/openssl.cnf
-	
+
+	# Modify the openssl.cnf file, to add our SAN's
+	sed -i "s/###IP###/$SERVER_IP/g" /etc/ssl/openssl.cnf
+
 	mkdir -p /tmp/certs && cd /tmp/certs
 
 	# Generate a new root key
 	openssl genrsa -out devdockerCA.key 2048
 	# Generate a new root certificate
-	openssl req -x509 -new -passin pass:${PASS_ROOT} -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${UNIT}/CN=${ROOT_COMMON_NAME}/emailAddress=${EMAIL}/SAN=*.${ROOT_COMMON_NAME}.xip.io" -nodes -key devdockerCA.key -days 10000 -out devdockerCA.crt
+	openssl req -x509 -new -passin pass:${PASS_ROOT} -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${UNIT}/CN=${ROOT_COMMON_NAME}/emailAddress=${EMAIL}" -nodes -key devdockerCA.key -days 10000 -out devdockerCA.crt
 	# Generate a key for server
 	openssl genrsa -out domain.key 2048
 	# Make a certificate signing request
-	openssl req -new -passin pass:${PASS_ROOT} -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${UNIT}/CN=${ROOT_COMMON_NAME}/emailAddress=${EMAIL}/SAN=*.${ROOT_COMMON_NAME}.xip.io" -key domain.key -out dev-docker-registry.csr
+	openssl req -new -passin pass:${PASS_ROOT} -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${UNIT}/CN=${ROOT_COMMON_NAME}/emailAddress=${EMAIL}" -key domain.key -out dev-docker-registry.csr
 	# Sign the certificate request
 	openssl x509 -req -in dev-docker-registry.csr -CA devdockerCA.crt -CAkey devdockerCA.key -CAcreateserial -out domain.crt -days 10000
 
